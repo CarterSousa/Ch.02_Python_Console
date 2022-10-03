a8b743c9f0fa4bf19ce244f68cdd12a1d3dbcaa2
CarterSousa <sousacar@urbandaleschools.com>
2022-10-03 12:15:04 -0500

    python looping

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -6,44 +6,46 @@ Sign your name:Carter Sousa
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
+'''
 
-b/a                 =2.5
-b//a                =2
-b**a                =25
-b%a                 =1
-a+B                 =Error: there is not a proper input because
-type(42)            =<class 'int'>
-type(42.0)          =<class 'float'>
-type("C3PO")        =<class 'str'>
-type(True)          =<class 'bool'>
-
-
+a=2
+b=5
+print(b/a)
+print(b//a)
+b**a
+b%a
+a+B
+type(42)
+type(42.0)
+type("C3PO")
+type(True)
+'''
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
-
+'''
 a=2
 a*=10
 a/=2
 a+=12
 a-=7
-a             =6.0
-type(a)       =<class 'float'>
+a
+type(a)
 
-
+'''
 4.) What is the mistake in the following code. Fix it!
-
+'''
 x,y=(4,5)
 a=3(x+y)
 a=
 
-
+'''
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
-
+'''
 x,y,z=(3,4,5)
 ave=x+y+z/3
 ave=
 
 
-'''
+
 #floor division
 
